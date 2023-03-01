89c4f9bc9d00f43c343aadfe47cf9b799eb430a5
Pawel1894 <pawepohl@gmail.com>
2023-03-01 21:45:41 +0100

    Desktop stylings for country page

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -17,19 +17,21 @@ export default function CountryCard({ flag, capital, name, population, region }:
       href={`/${name}`}
       className="block mt-6 mb-12 rounded-md shadow-lg w-[16.5rem] min-h-[21rem] h-fit dark:bg-neutral-100 hover:-translate-y-2 transition-transform"
     >
-      <div className="relative w-full h-[160px]">
+      <div className="relative w-full h-[10rem]">
         <Image className="rounded-t-md object-cover" src={flag} alt={`flag of ${name}`} fill />
       </div>
       <div className="mx-6 mt-6">
         <h2 className="text-lg font-extrabold text-neutral-300 dark:text-white mb-4">{name}</h2>
         <CountryField label="Population">
-          <span className="text-sm font-light text-neutral-300 dark:text-white">{population}</span>
+          <span className="text-sm xl:text-base font-light text-neutral-300 dark:text-white">
+            {population}
+          </span>
         </CountryField>
         <CountryField className={"mt-2"} label="Region">
-          <span className="text-sm font-light text-neutral-300 dark:text-white">{region}</span>
+          <span className="text-sm xl:text-base font-light text-neutral-300 dark:text-white">{region}</span>
         </CountryField>
         <CountryField className={"mt-2"} label="Capital">
-          <span className="text-sm font-light text-neutral-300 dark:text-white">{capital}</span>
+          <span className="text-sm xl:text-base font-light text-neutral-300 dark:text-white">{capital}</span>
         </CountryField>
       </div>
     </Link>
--- components/UI/Heading.tsx
+++ components/UI/Heading.tsx
@@ -9,13 +9,13 @@ type Props = {
 
 export default function Heading({ font }: Props) {
   return (
-    <header
-      className={`${font.variable} font-sans flex justify-between px-4 py-[1.875rem] dark:bg-neutral-100 lg:px-20 lg:py-6`}
-    >
-      <h1 className="font-extrabold text-sm text-neutral-300 dark:text-white lg:text-2xl">
-        <Link href={"/"}>Where in the world?</Link>
-      </h1>
-      <ThemeToggle />
+    <header className={`${font.variable} font-sans px-4 py-[1.875rem] dark:bg-neutral-100 lg:px-20 lg:py-6 `}>
+      <div className="max-w-[79.875rem] flex justify-between mx-auto">
+        <h1 className="font-extrabold text-sm text-neutral-300 dark:text-white lg:text-2xl">
+          <Link href={"/"}>Where in the world?</Link>
+        </h1>
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
